c6c3416a067a317afa1104aaeb16520e9183cab4
KTest111 <htet26783@gmail.com>
2023-07-15 21:26:03 +0630

    first commit

--- CodeWall/src/Chapter3_Arrays/Switch_2_character.java
+++ CodeWall/src/Chapter3_Arrays/Switch_2_character.java
@@ -1,2 +1,19 @@
-package Chapter3_Arrays;public class Switch_2_character {
+package Chapter3_Arrays;
+
+import java.util.Scanner;
+
+public class Switch_2_character {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the first number =");
+        int a = scanner.nextInt();
+        System.out.println("Enter the second number =");
+        int b = scanner.nextInt();
+        System.out.println("Before  a =" + a + ", b =" + b);
+
+        int c = a;
+        a = b;
+        b = c;
+        System.out.println("After  a =" + a + ", b =" + b);
+    }
 }
